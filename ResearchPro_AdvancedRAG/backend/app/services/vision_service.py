7ccaaac95b0883ab6aac88b093908b9b462bffd5
Soham Datta <dattasoham05@gmail.com>
2026-04-14 12:01:04 +0530

    added markdown text spliiter

--- ResearchPro_AdvancedRAG/backend/app/services/vision_service.py
+++ ResearchPro_AdvancedRAG/backend/app/services/vision_service.py
@@ -3,14 +3,14 @@
 from docling.document_converter import DocumentConverter
 from langchain.schema import Document
 from langchain_core.messages import HumanMessage, SystemMessage
-from config.config import llm_summarize ,chunking_tokenizer
-import re
-from docling.chunking import HybridChunker
+#from config.config import llm_summarize 
+#from docling.chunking import HybridChunker
+from langchain.text_splitter import MarkdownHeaderTextSplitter
 
 
 class MultimodalProcessor:
-    def __init__(self):
-        self.llm = llm_summarize
+    # def __init__(self):
+    #     self.llm = llm_summarize   
         
     def load_and_process(self, filepath:str) -> list[Document]:
         try:
@@ -20,28 +20,35 @@ class MultimodalProcessor:
             docling_result = DocumentConverter().convert(filepath)
             docling_doc = docling_result.document 
             
-            #intialize chunker 
-            chunker = HybridChunker(
-                tokenizer=chunking_tokenizer,
-                merge_peers = True)
+            # #intialize chunker 
+            # chunker = HybridChunker(
+            #     tokenizer=chunking_tokenizer,
+            #     merge_peers = False) #to not exceed token limit
             
-            #chunking 
-            chunks = list(chunker.chunk(dl_doc=docling_doc))
+            # #chunking 
+            # chunks = list(chunker.chunk(dl_doc=docling_doc))
+            markdown_text = docling_doc.export_to_markdown()
             
-                        
+            headers_to_split_on = [
+                ("#", "Header 1"),
+                ("##", "Header 2"),
+                ("###", "Header 3"),
+            ]
+            splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+            chunks = splitter.split_text(markdown_text)
+            
+            
             #pass as langchain documents
             processed_langchain_docs = []
             for chunk in chunks:
                 doc = Document(
-                    page_content=chunk.text,
-                    metadata={
-                        "headings": chunk.meta.headings,
-                        "doc_items": [item.label for item in chunk.meta.doc_items]
-                    }
+                    page_content=chunk.page_content,
+                    metadata=chunk.metadata
                 )
                 processed_langchain_docs.append(doc)
             
             return processed_langchain_docs
+        
         except Exception as e:
             import traceback
             print(traceback.format_exc())
